7d0a28f67d515932f7d5ef36002a70f030d6703f
alex-junio <aleks.junio@gmail.com>
2022-03-15 15:22:53 -0300

    exercicio 3

---
+++ aula01/Exercicio3/src/Exercicio3.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+/*
+ * Escreva um programa que leia um número inteiro digitado pelo usuário e exiba uma sequência de números de 1 até o valor digitado pelo usuário. 
+Por exemplo, se o usuário digitar o número 8, o programa deve mostrar
+1 2 3 4 5 6 7 8
+*/
+
+//versão 1 - usando while
+public class Exercicio3 {
+	public static void main(String[] args) {
+		System.out.println("Escolha um número para ver o que vem antes dele.");
+		System.out.println(
+				"O programa mostrará os números anteriores ao que foi digitado e, só porque sim, mostrará o número digitado também. Aproveita que é de graça.");
+		int numero, contador = 0;
+		Scanner teclado = new Scanner(System.in);
+		System.out.println("Digite um número, por favor.");
+		numero = teclado.nextInt();
+		teclado.close();
+		while (contador <= numero) {
+			System.out.println(contador);
+			contador++;
+		}
+		;
+	}
+}
