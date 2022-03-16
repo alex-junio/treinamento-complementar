ee2a28b38ba8ea30d05f6e2f01a1be3c68452dc1
alex-junio <aleks.junio@gmail.com>
2022-03-16 15:30:58 -0300

    aula02, exercicio 1

---
+++ Aula02/Exercicio1/src/AppVeiculo.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class AppVeiculo {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		String marca, modelo;
+		double consumo;
+		System.out.println("Qual a marca?");
+		marca = teclado.nextLine();
+		System.out.println("Qual o modelo?");
+		modelo = teclado.nextLine();
+		System.out.println("Quantos quilômetros por litro?");
+		consumo = teclado.nextDouble();
+
+		Veiculo v1 = new Veiculo(marca, modelo, consumo);
+		v1.mostrar();
+		teclado.close();
+	}
+}
---
+++ Aula02/Exercicio1/src/Veiculo.java
@@ -0,0 +1,19 @@
+
+public class Veiculo {
+	private String marca, modelo;
+	private double consumo;
+
+	public Veiculo(String marca, String modelo, double consumo) {
+		this.marca = marca;
+		this.modelo = modelo;
+		this.consumo = consumo;
+	}
+
+	public void mostrar() {
+		System.out.println("Marca: " + marca + " Modelo: " + modelo + " Consumo: " + consumo);
+	}
+
+	public double getConsumo() {
+		return consumo;
+	}
+}
