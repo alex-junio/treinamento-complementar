e52ebb4b53f7b90f2a72114199ada34027087b1f
alex-junio <aleks.junio@gmail.com>
2022-03-18 00:01:01 -0300

    aula 2, relogio

---
+++ Aula02/ProjetoRelogio/src/relogio/AppRelogio.java
@@ -0,0 +1,22 @@
+package relogio;
+
+import java.util.Scanner;
+
+public class AppRelogio {
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		int hora, minuto, segundo;
+		System.out.println("Quantas horas?");
+		hora = teclado.nextInt();
+		System.out.println("Quantos minutos?");
+		minuto = teclado.nextInt();
+		System.out.println("Quantos segundos?");
+		segundo = teclado.nextInt();
+		Relogio relogio1 = new Relogio(hora, minuto, segundo);
+
+		relogio1.setHora(hora);
+		relogio1.setMinuto(minuto);
+		relogio1.setSegundo(segundo);
+		System.out.println(relogio1.getHoraAtual());
+	}
+}
