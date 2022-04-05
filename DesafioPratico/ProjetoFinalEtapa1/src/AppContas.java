3063a3c0ec3dcffde536737c71650b58524ee393
alex-junio <aleks.junio@gmail.com>
2022-04-04 21:44:46 -0300

    alteracoes em nomes de variaveis e trechos de codigo que so faziam sentido no modelo original

--- DesafioPratico/ProjetoFinalEtapa1/src/AppContas.java
+++ DesafioPratico/ProjetoFinalEtapa1/src/AppContas.java
@@ -15,22 +15,22 @@ public class AppContas {
 
 		int opcao = 0;
 		int numeroConta;
-		String menu = "O que você deseja fazer?\r\n1: criar conta;\r\n2: deletar conta;\r\n3: buscar;\r\n4: depositar;\r\n5: sacar;\r\n6: sair;";
-		String menuCriarConta = "Qual o tipo da conta? \r\n1: conta corrente;\r\n2: conta especial;";
+		String menu = "O que você deseja fazer?\r\n1: Criar conta;\r\n2: Remover conta;\r\n3: Buscar;\r\n4: Depositar;\r\n5: Sacar;\r\n6: Sair;";
+		String menuCriarConta = "Qual o tipo da conta? \r\n1: Conta Corrente;\r\n2: Conta Especial;";
 
 		while (opcao != 6) {
 			System.out.println(menu);
-			opcao = Integer.parseInt(teclado.nextLine());
+			opcao = teclado.nextInt();
 
 			switch (opcao) {
 			case 1: {
 				Boolean criouConta = true;
 
 				System.out.println(menuCriarConta);
-				opcao = Integer.parseInt(teclado.nextLine());
+				opcao = teclado.nextInt();
 
 				System.out.println("digite o numero para a conta");
-				numeroConta = Integer.parseInt(teclado.nextLine());
+				numeroConta = teclado.nextInt();
 
 				switch (opcao) {
 				case 1:
@@ -43,9 +43,9 @@ public class AppContas {
 					break;
 
 				case 2:
-					System.out.println("Conta especial. Digite o valor limite");
-					int valorLimite = Integer.parseInt(teclado.nextLine());
-					ce = new ContaEspecial(numeroConta, valorLimite);
+					System.out.println("Conta especial. Digite o limite");
+					int limite = teclado.nextInt();
+					ce = new ContaEspecial(numeroConta, limite);
 					if (gc.adicionar(ce) == true) {
 						criouConta = true;
 					} else {
@@ -66,9 +66,9 @@ public class AppContas {
 
 			case 2: {
 				System.out.println("digite o numero da conta");
-				numeroConta = Integer.parseInt(teclado.nextLine());
+				numeroConta = teclado.nextInt();
 				if (gc.remover(numeroConta) == true) {
-					System.out.println("conta deletada!");
+					System.out.println("conta removida!");
 				} else {
 					System.out.println("erro! Conta não encontrada...");
 				}
@@ -76,52 +76,52 @@ public class AppContas {
 			}
 
 			case 3: {
-				System.out.println("numero da conta para buscar");
-				numeroConta = Integer.parseInt(teclado.nextLine());
+				System.out.println("número da conta para buscar");
+				numeroConta = teclado.nextInt();
 				System.out.println(gc.mostrarDados(numeroConta));
 				break;
 			}
 
 			case 4: {
-				System.out.println("digite o número da conta que será feito o depósito");
-				numeroConta = Integer.parseInt(teclado.nextLine());
+				System.out.println("digite o número da conta onde será feito o depósito");
+				numeroConta = teclado.nextInt();
 				System.out.println("valor do depósito");
-				double valorADepositar = Double.parseDouble(teclado.nextLine());
+				double valorDeposito = teclado.nextDouble();
 				int posicao = gc.getPosicao(numeroConta);
 				if (posicao < 0) {
 					System.out.println("erro! conta não encontrada...");
 				} else {
-					gc.contas.get(posicao).depositar(valorADepositar);
-					System.out.println("depósito de " + valorADepositar + " realizado");
+					gc.contas.get(posicao).depositar(valorDeposito);
+					System.out.println("depósito de " + valorDeposito + " realizado");
 				}
 				break;
 			}
 
 			case 5: {
 				System.out.println("digite o número da conta que será feito o saque");
-				numeroConta = Integer.parseInt(teclado.nextLine());
+				numeroConta = teclado.nextInt();
 				System.out.println("valor do saque");
-				double valorASacar = Double.parseDouble(teclado.nextLine());
+				double valorSaque = teclado.nextDouble();
 				int posicao = gc.getPosicao(numeroConta);
 				if (posicao < 0) {
 					System.out.println("erro! conta não encontrada...");
 					break;
 				}
-				if (gc.contas.get(posicao).sacar(valorASacar)) {
-					System.out.println("saque de " + valorASacar + " realizado");
+				if (gc.contas.get(posicao).sacar(valorSaque)) {
+					System.out.println("saque de " + valorSaque + " realizado");
 				} else {
-					System.out.println("limite da conta não permite este saque...");
+					System.out.println("O limite da conta não permite este saque.");
 				}
 				break;
 			}
 
 			case 6: {
-				System.out.println("finalizando sistema...");
+				System.out.println("Saindo...");
 				break;
 			}
 
 			default: {
-				System.out.println("comando desconhecido");
+				System.out.println("Opção inválida!");
 				break;
 			}
 			}
